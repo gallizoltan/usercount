061e63241978c18ce5d5c38b9dacade7074bb62e
Gálli Zoltán <gallizoltan@gmail.com>
2018-08-17 12:53:32 +0200

    helper script to handle usercount logging

---
+++ usercount.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+scritp_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $scritp_dir
+echo -n "Starting at: " | tee -a usercount.log
+date +"%Y-%m-%d %H:%M:%S" | tee -a usercount.log
+stdbuf -o L ./usercount.py $1 2>&1 | tee -a usercount.log
+tail -9998 usercount.log > usercount.log.temp
+mv usercount.log.temp usercount.log
