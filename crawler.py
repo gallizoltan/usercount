9c15704031ae95e2308f6c81805c5053f42fc18b
Gálli Zoltán <gallizoltan@gmail.com>
2018-08-30 00:58:12 +0200

    crawler multiprocessing

--- crawler.py
+++ crawler.py
@@ -3,6 +3,10 @@
 import json
 import os
 import requests
+from multiprocessing import Process
+import multiprocessing
+import time
+import copy
 
 file_path = "instances.json"
 instances = {}
@@ -10,36 +14,82 @@ if os.path.isfile(file_path):
 	with open( file_path ) as f:
 		instances = json.load(f)
 
-toots_count = 0
-user_count = 0
-instance_count = 0
-for j in range(len(instances)):
-	i = instances[j]
+global_timeout = 30
+
+def exception_wrapper(procnum, name, return_dict):
+	rv = {}
+	rv['status_count'] = 0
+	rv['user_count'] = 0
+	rv['active'] = 0
 	try:
-		page = requests.get("https://" + i["name"] + "/api/v1/instance", timeout=1)
+		page = requests.get("https://" + name + "/api/v1/instance", timeout=global_timeout)
 		instance = json.loads(page.content.decode('utf-8'))
-		#print(instance)
 		toots = int(instance['stats']['status_count'])
 		users = int(instance['stats']['user_count'])
-		print("%s of %s %s: users: %s toots: %s"%(str(j), str(len(instances)), i["name"], str(users), str(toots)))
-		toots_count += toots
-		user_count += users
-		instance_count += 1
-	except Exception as e:
-		print("%s of %s"%(str(j), str(len(instances))))
-		#print(e)
+		#print("%s %s: users: %s toots: %s"%(procnum, name, str(users), str(toots)))
+		rv['status_count'] = toots
+		rv['user_count'] = users
+		rv['active'] = 1
+	except:
 		pass
+	return_dict[procnum] = rv
+
+def start_proc(jobs):
+	print("starting processes: " + str(len(jobs)))
+	for j in jobs:
+		j.start()
+
+def end_proc(jobs):
+	if len(jobs) > 0:
+		time.sleep(global_timeout + 1)
+		print("ending processes: " + str(len(jobs)))
+		for j in jobs:
+			try:
+				j.terminate()
+				j.join()
+			except Exception as e:
+				print(e)
+
+processes = []
+manager = multiprocessing.Manager()
+return_dict = manager.dict()
+for j in range(len(instances)):
+	i = instances[j]
+	if "name" in i:
+		processes.append(Process(target=exception_wrapper, args=(j, i["name"], return_dict)))
+	if (j+1) % 800 == 0:
+		start_proc(processes)
+		end_proc(processes)
+		processes = []
+
+start_proc(processes)
+end_proc(processes)
+
+return_dict = copy.deepcopy(return_dict)
+
+user_count = 0
+toots_count = 0
+instance_count = 0
+for i in return_dict:
+	rv = return_dict[i]
+	user_count += rv['user_count']
+	toots_count += rv['status_count']
+	instance_count += rv['active']
 
 stats = {}
-stats["toots_count"] = toots_count
-stats["user_count"] = user_count
-stats["instance_count"] = instance_count
+stats_file = "stats.json"
+if os.path.isfile(stats_file):
+		with open( stats_file ) as f:
+			stats = json.load(f)
+
+current_ts = str(int(time.time()))
+stats[current_ts] = {}
+stats[current_ts]["toots_count"] = toots_count
+stats[current_ts]["user_count"] = user_count
+stats[current_ts]["instance_count"] = instance_count
 print("toots_count %s user_count %s instance_count %s"%(toots_count, user_count, instance_count))
 print(stats)
 
-stats_file = "stats2.json"
 with open(stats_file, 'w') as outfile:
 	json.dump(stats, outfile, indent=4, sort_keys=True)
-
-
 
