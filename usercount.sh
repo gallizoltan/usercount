e7e924584027bc5b172b530fbdb07a17271b106d
Gálli Zoltán <gallizoltan@gmail.com>
2018-08-30 13:12:23 +0200

    regular backup

--- usercount.sh
+++ usercount.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 scritp_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $scritp_dir
+
 echo -n "Starting at: " | tee -a usercount.log
 date +"%Y-%m-%d %H:%M:%S" | tee -a usercount.log
 (stdbuf -o L ./usercount.py $@ 3>&1 1>&2 2>&3 | tee -a usercount.err) 2>&1  | tee -a usercount.log
 tail -9998 usercount.log > usercount.log.temp
 mv usercount.log.temp usercount.log
+
+BACKUP=`grep backup_folder config.txt | cut -f2 -d":" | tr -d '\" '`
+if [ "$BACKUP" != "" ]
+then
+	cp mastostats.csv "$BACKUP"
+	cp snapshot.json "$BACKUP"
+	cp list.json "$BACKUP"
+fi
