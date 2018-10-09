5f3b05b2dc0f70c3033b2f231b848ecfca484bfc
Gálli Zoltán <gallizoltan@gmail.com>
2018-10-09 09:08:44 +0200

    rotate crawler.log file too

--- usercount.sh
+++ usercount.sh
@@ -5,8 +5,11 @@ cd $scritp_dir
 echo -n "Starting at: " | tee -a usercount.log
 date +"%Y-%m-%d %H:%M:%S" | tee -a usercount.log
 (stdbuf -o L ./usercount.py $@ 3>&1 1>&2 2>&3 | tee -a usercount.err) 2>&1  | tee -a usercount.log
+
 tail -9998 usercount.log > usercount.log.temp
 mv usercount.log.temp usercount.log
+tail -998 crawler.log > crawler.log.temp
+mv crawler.log.temp crawler.log
 
 BACKUP=`grep backup_folder config.txt | cut -f2 -d":" | tr -d '\" ,'`
 if [ "$BACKUP" != "" ]
