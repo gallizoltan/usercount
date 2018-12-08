d69bc290a38b50a63388b7edba3267ce809b003e
Gálli Zoltán <gallizoltan@gmail.com>
2018-12-08 17:49:14 +0000

    filtering suspicious logs to different file
    - suspicious logs redirected to crawler.err file
    - preserve log line number is configurable
    - preserve more logs by default

--- crawler.sh
+++ crawler.sh
@@ -5,5 +5,8 @@ cd $scritp_dir
 killall crawler.py
 stdbuf -o L ./crawler.py $@ 2>&1 | tee -a crawler.log
 
-tail -998 crawler.log > crawler.log.temp
+LOGLINES=$(grep loglines "config.txt" | cut -f2 -d":" | cut -f2 -d"\"")
+[ -z "$LOGLINES" ] && LOGLINES="9998"
+tail -n $LOGLINES crawler.log > crawler.log.temp
 mv crawler.log.temp crawler.log
+cat crawler.log | grep -v "+" | grep -v "No more time left" | grep -v " done$" | rev | cut -d$'\r' -f 1 | rev | uniq -f 2 > crawler.err
