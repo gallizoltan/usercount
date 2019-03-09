9402444544f73a47f2dfe4f6b661605eac8c3ed9
Gálli Zoltán <gallizoltan@gmail.com>
2019-03-09 07:11:59 +0000

    usercount.x files become publish.x
    - this name better describes the functionality

--- publish.sh
+++ publish.sh
@@ -2,12 +2,12 @@
 scritp_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $scritp_dir
 
-echo -n "Starting at: " | tee -a usercount.log
-date +"%Y-%m-%d %H:%M:%S" | tee -a usercount.log
-(stdbuf -o L ./usercount.py $@ 3>&1 1>&2 2>&3 | tee -a usercount.err) 2>&1  | tee -a usercount.log
+echo -n "Starting at: " | tee -a publish.log
+date +"%Y-%m-%d %H:%M:%S" | tee -a publish.log
+(stdbuf -o L ./publish.py $@ 3>&1 1>&2 2>&3 | tee -a publish.err) 2>&1  | tee -a publish.log
 
-tail -9998 usercount.log > usercount.log.temp
-mv usercount.log.temp usercount.log
+tail -9998 publish.log > publish.log.temp
+mv publish.log.temp publish.log
 
 BACKUP=`grep backup_folder config.txt | cut -f2 -d":" | tr -d '\" ,'`
 if [ "$BACKUP" != "" ]
