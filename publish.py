0ad93116ce20b49068ef4df9a152d03d8f1c76c2
Gálli Zoltán <gallizoltan@gmail.com>
2019-03-09 14:17:32 +0100

    refactor: publish gets a main function

--- publish.py
+++ publish.py
@@ -6,9 +6,6 @@ import json, csv
 import os, sys
 import time
 
-# config.txt, mastostats.csv, generate.gnuplot, etc. are in the same folder as this file
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
 def get_config(filename):
     if os.path.isfile(filename):
         with open( filename ) as f:
@@ -26,14 +23,6 @@ def get_mastodon(config_file):
     )
     return mastodon
 
-# Load CSV file
-mastostats_csv = "mastostats.csv"
-with open(mastostats_csv, 'r') as csvfile:
-	reader = csv.reader(csvfile)
-	masto_array = [row for row in reader]
-csvfile.close()
-del masto_array[0]
-
 def find_closest_timestamp(input_array, seek_timestamp):
 	closest = input_array[-1]
 	for item in reversed(input_array):
@@ -43,51 +32,64 @@ def find_closest_timestamp(input_array, seek_timestamp):
 			return closest
 	return closest
 
-ts = int(time.time())
-current_val = find_closest_timestamp(masto_array, ts)
-user_count = int(current_val[1])
+def main():
+	# config.txt, mastostats.csv, generate.gnuplot, etc. are in the same folder as this file
+	os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-print("Number of users: %s " % user_count)
-print("Number of toots: %s " % current_val[3])
-print("Number of instances: %s " % current_val[2])
+	# Load CSV file
+	mastostats_csv = "mastostats.csv"
+	with open(mastostats_csv, 'r') as csvfile:
+		reader = csv.reader(csvfile)
+		masto_array = [row for row in reader]
+	csvfile.close()
+	del masto_array[0]
 
-toot_text = format(user_count, ",d") + " accounts \n"
-one_hour = 60 * 60
-hours = [1, 24, 168]
-prefix = ["Hourly", "Daily", "Weekly"]
-suffix = ["hour", "day", "week"]
+	ts = int(time.time())
+	current_val = find_closest_timestamp(masto_array, ts)
+	user_count = int(current_val[1])
 
-# Calculate difference in times
-for i in range(3):
-    if len(masto_array) <= hours[i]: continue
-    old_ts = ts - hours[i] * one_hour
-    old_val = find_closest_timestamp(masto_array, old_ts)
-    change = user_count - int(old_val[1])
-    print("%s change %s" % (prefix[i], change))
-    if change > 0:
-        toot_text += "+" + format(change, ",d") + " in the last " + suffix[i] + "\n"
+	print("Number of users: %s " % user_count)
+	print("Number of toots: %s " % current_val[3])
+	print("Number of instances: %s " % current_val[2])
 
-# Generate chart
-FNULL = open(os.devnull, 'w')
-call(["gnuplot", "generate.gnuplot"])
+	toot_text = format(user_count, ",d") + " accounts \n"
+	one_hour = 60 * 60
+	hours = [1, 24, 168]
+	prefix = ["Hourly", "Daily", "Weekly"]
+	suffix = ["hour", "day", "week"]
 
-# Upload chart
-file_to_upload = 'graph.png'
-mastodon = get_mastodon(config_file = "config.txt")
+	# Calculate difference in times
+	for i in range(3):
+		if len(masto_array) <= hours[i]: continue
+		old_ts = ts - hours[i] * one_hour
+		old_val = find_closest_timestamp(masto_array, old_ts)
+		change = user_count - int(old_val[1])
+		print("%s change %s" % (prefix[i], change))
+		if change > 0:
+			toot_text += "+" + format(change, ",d") + " in the last " + suffix[i] + "\n"
 
-media_dict = None
-try:
-    print("Uploading %s..."%file_to_upload)
-    media_dict = mastodon.media_post(file_to_upload)
+	# Generate chart
+	FNULL = open(os.devnull, 'w')
+	call(["gnuplot", "generate.gnuplot"])
 
-    print("Uploaded file, returned:")
-    print(str(media_dict))
-except Exception as e:
-    print("Exception while uploading: " + str(e), file=sys.stderr)
+	# Upload chart
+	file_to_upload = 'graph.png'
+	mastodon = get_mastodon(config_file = "config.txt")
 
-print("Tooting...")
-print(toot_text, end='')
+	media_dict = None
+	try:
+		print("Uploading %s..."%file_to_upload)
+		media_dict = mastodon.media_post(file_to_upload)
 
-mastodon.status_post(toot_text, in_reply_to_id=None, media_ids=[media_dict] )
+		print("Uploaded file, returned:")
+		print(str(media_dict))
+	except Exception as e:
+		print("Exception while uploading: " + str(e), file=sys.stderr)
 
-print("Successfully tooted!")
+	print("Tooting...")
+	print(toot_text, end='')
+	mastodon.status_post(toot_text, in_reply_to_id=None, media_ids=[media_dict] )
+	print("Successfully tooted!")
+
+if __name__ == "__main__":
+	main()
