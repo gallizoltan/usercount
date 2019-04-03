34971b1ddbfb72e4c20a5f4f20693459cf43c3e2
Gálli Zoltán <gallizoltan@gmail.com>
2019-04-03 11:20:54 +0000

    running time format is MM:SS

--- crawler.sh
+++ crawler.sh
@@ -10,10 +10,11 @@ do
 	stdbuf -o L ./crawler.py $@ 2>&1 | tee -a crawler.log
 done
 
-echo $(date +"%Y-%m-%d %H:%M:%S")" + Crawler Quadruple finised in $(($(date '+%s') - $STARTTS)) s" | tee -a crawler.log
-
 LOGLINES=$(grep loglines "config.txt" | cut -f2 -d":" | cut -f2 -d"\"")
 [ -z "$LOGLINES" ] && LOGLINES="9998"
 tail -n $LOGLINES crawler.log > crawler.log.temp
 mv crawler.log.temp crawler.log
 tac crawler.log | grep -v "+" | grep -v "[Nn]o more time left" | grep -v " done$" | rev | cut -d$'\r' -f 1 | rev | uniq -f 2 | tac > crawler.err
+
+RUNNING_TIME=$(($(date '+%s') - $STARTTS))
+echo $(date +"%Y-%m-%d %H:%M:%S")" + Crawler Quadruple finised in "$(date -d @$RUNNING_TIME +"%M:%S") | tee -a crawler.log
