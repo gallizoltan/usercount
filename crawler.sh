f47630d30eee589f5124f47fd9f3812f630275eb
Gálli Zoltán <gallizoltan@gmail.com>
2021-09-23 14:54:53 +0200

    compile and run python files

--- crawler.sh
+++ crawler.sh
@@ -4,12 +4,16 @@ cd $scritp_dir
 
 export TZ=Europe/Budapest
 
+python3 -m compileall -l . > /dev/null
+cp __pycache__/common.cpython*.pyc common.pyc
+cp __pycache__/crawler.cpython*.pyc crawler.pyc
+
 echo $(date +"%Y-%m-%d %H:%M:%S")" + Crawler Quadruple started" | tee -a crawler.log
 STARTTS=$(date +'%s')
 killall crawler.py 2>/dev/null
 for i in $(seq 1 4);
 do
-	stdbuf -o L ./crawler.py $@ 2>&1 | tee -a crawler.log
+	stdbuf -o L python3 crawler.pyc $@ 2>&1 | tee -a crawler.log
 done
 
 LOGLINES=$(grep loglines "config.txt" | cut -f2 -d":" | cut -f2 -d"\"")
