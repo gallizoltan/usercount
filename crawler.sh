221d449532ceac14623c6992db52c4b7c3fd8443
Gálli Zoltán <gallizoltan@gmail.com>
2022-09-01 13:24:06 +0200

    fix execcount at startup

--- crawler.sh
+++ crawler.sh
@@ -8,12 +8,18 @@ python3 -m compileall -l . > /dev/null
 cp __pycache__/common.cpython*.pyc common.pyc
 cp __pycache__/crawler.cpython*.pyc crawler.pyc
 
-echo $(date +"%Y-%m-%d %H:%M:%S")" + Crawler Quadruple started" | tee -a crawler.log
+EXECCOUNT=$(grep -oP "execcount[^\-0-9]*\K[\-0-9]*" snapshot.json)
+echo $(date +"%Y-%m-%d %H:%M:%S")" + Crawler Quadruple started with execcount $EXECCOUNT" | tee -a crawler.log
+if [ $(($EXECCOUNT % 4)) -ne 0 ]; then
+    EXECCOUNT2=$(($EXECCOUNT+4-$(($EXECCOUNT%4))))
+    sed -i "s/\"execcount\": $EXECCOUNT,/\"execcount\": $EXECCOUNT2,/g" snapshot.json
+fi
+
 STARTTS=$(date +'%s')
 killall crawler.py 2>/dev/null
 for i in $(seq 1 4);
 do
-	stdbuf -o L python3 crawler.pyc $@ 2>&1 | tee -a crawler.log
+    stdbuf -o L python3 crawler.pyc $@ 2>&1 | tee -a crawler.log
 done
 
 LOGLINES=$(grep loglines "config.txt" | cut -f2 -d":" | cut -f2 -d"\"")
