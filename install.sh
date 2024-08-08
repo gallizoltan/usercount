b5f5f4a091a32848b356be49fdc2324fa7c1dfc3
Gálli Zoltán <git@zoltan.xyz>
2024-08-08 10:44:46 +0000

    install script update

--- install.sh
+++ install.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be root" 2>&1
-  exit 1
-fi
-
-apt update
-apt install -y tor
-apt install -y python3-pip
-apt install -y gnuplot-qt
-usermod -a -G debian-tor $(whoami)
-service tor restart
+sudo apt install -y python3-pip gnuplot-qt
 
 pip3 install Mastodon.py
-pip3 install pysocks
 pip3 install psutil
