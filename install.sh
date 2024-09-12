74270e902404c843f0b9364edae3bc41ea3c9ab3
Gálli Zoltán <git@zoltan.xyz>
2024-09-12 13:59:53 +0000

    new install tweeks

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+sudo apt update
+
 sudo apt install -y python3-pip gnuplot-qt
+
+# tipp: sudo mv /usr/lib/python3.12/EXTERNALLY-MANAGED /usr/lib/python3.12/EXTERNALLY-MANAGED.old
 
 pip3 install Mastodon.py
 pip3 install psutil
